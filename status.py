9f9270ede79fbb6b70563f94ebd64f71cd36d4c5
NelioChume <74329121+NelioChume@users.noreply.github.com>
2023-12-04 11:23:07 +0200

    add: show databases

--- status.py
+++ status.py
@@ -1,39 +1,39 @@
 import subprocess
 import json
 
-# Função para verificar o estado do MySQL em um contêiner
-def check_mysql_status(container_name):
-    print("----------------------------------------")
-    print(f"Verificando o estado do MySQL no contêiner {container_name}...")
 
-    # Verifica se o MySQL está instalado
+# Função para listar bases de dados em um contêiner MySQL
+def list_user_databases(container_name):
+    # Verifica se o MySQL está instalado e ativo
     try:
-        subprocess.run(["lxc", "exec", container_name, "--", "which", "mysql"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(f"MySQL está instalado no contêiner {container_name}.")
+        subprocess.run(["lxc", "exec", container_name, "--", "systemctl", "is-active", "mysql"], check=True,
+                       stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print("----------------------------------------")
+        print(f"Listando bases de dados no contêiner {container_name}...")
 
-        # Verifica se o serviço MySQL está em execução
-        try:
-            subprocess.run(["lxc", "exec", container_name, "--", "systemctl", "is-active", "mysql"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            print(f"MySQL está ativo no contêiner {container_name}.")
-        except subprocess.CalledProcessError:
-            print(f"MySQL não está ativo no contêiner {container_name}.")
+        # Lista as bases de dados
+        subprocess.run(["lxc", "exec", container_name, "--", "mysql", "-e", "SHOW DATABASES;"], check=True)
 
     except subprocess.CalledProcessError:
-        print(f"MySQL não está instalado no contêiner {container_name}.")
+        pass  # MySQL não está instalado ou ativo, não faz nada
+
 
 # Função principal
 def main():
-    # Obtém nomes dos contêineres que estão ligados
-    containers_raw = subprocess.run(["lxc", "list", "--format", "json"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # Obtém nomes dos contêineres que têm o MySQL instalado e ativo
+    containers_raw = subprocess.run(["lxc", "list", "--format", "json"], check=True, stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
     containers_data = json.loads(containers_raw.stdout.decode("utf-8"))
     containers = [container["name"] for container in containers_data if container["status"] == "Running"]
 
-    # Itera sobre a lista de contêineres e realiza verificações
+    # Itera sobre a lista de contêineres e lista as bases de dados do usuário
     for container_name in containers:
-        check_mysql_status(container_name)
+        list_user_databases(container_name)
 
     print("----------------------------------------")
+
 
 # Chama a função main se este script for o programa principal
 if __name__ == "__main__":
     main()
+

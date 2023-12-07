c615be0b4d4da5d48d6436d11adc9afbda55ec55
NelioChume <74329121+NelioChume@users.noreply.github.com>
2023-12-07 12:15:30 +0200

    add: new files

---
+++ teste.py
@@ -0,0 +1,22 @@
+import paramiko
+from datetime import datetime
+import json
+import os
+from dotenv import load_dotenv
+
+def main():
+    # Informações de conexão SSH
+    hostname = 'apps.hisplp.org'
+    username = 'nelio'
+    password = 'ibrahimovic'
+    port = 384
+
+    client = paramiko.client.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=hostname, port=port, username=username, password=password)
+    _stdin, _stdout, _stderr = client.exec_command("df")
+    print(_stdout.read().decode())
+    client.close()
+
+if __name__ == "__main__":
+    main()

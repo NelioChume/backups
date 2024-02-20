7b11f2ecb4fb482a88b26435253f9f2058a5ec5c
NelioChume <74329121+NelioChume@users.noreply.github.com>
2024-02-20 16:33:03 +0200

    main2

--- bk/main.py
+++ bk/main.py
@@ -9,59 +9,75 @@ load_dotenv()
 local_backup_dir = os.getenv('LOCAL_BACKUP_DIR')
 remote_backup_dir = os.getenv('REMOTE_BACKUP_DIR')
 
-# Lista de containers LXC/LXD para excluir
+# Containers LXC/LXD excluídos
 excluded_containers = ["postgres", "monitor", "proxy"]
 
-# Lista de bancos de dados MySQL para excluir
-excluded_mysql_databases = ['information_schema', 'mysql', 'performance_schema', 'sys']
+# Bancos de dados excluídos
+excluded_databases = {
+    "mysql": ['information_schema', 'mysql', 'performance_schema', 'sys'],
+    "postgres": ['postgres', 'template0', 'template1']
+}
 
-def backup_postgres_databases(container_name):
+def run_command(command):
     try:
-        output = subprocess.check_output(["lxc", "exec", container_name, "--", "bash", "-c",
-                                          "psql -U postgres -l | awk '{print $1}' | grep -vE '(template0|template1|postgres)'"]).decode()
-        databases = output.split("\n")[2:-1]  # Remove as primeiras duas linhas e a última linha
-
-        for db in databases:
-            timestamp = datetime.now().strftime("%d-%m-%Y")
-            backup_file = f"bk_{db}_{timestamp}.sql.gz"
-            backup_path = os.path.join(local_backup_dir, backup_file)
-            subprocess.run(["lxc", "exec", container_name, "--", "bash", "-c",
-                            f"pg_dump -U postgres {db} | gzip > {backup_path}"])
-            print(f"Backup do banco de dados {db} concluído com sucesso.")
+        subprocess.run(command, check=True)
     except subprocess.CalledProcessError:
-        print(f"Erro ao fazer backup do banco de dados no container {container_name}.")
+        print(f"Erro ao executar o comando: {' '.join(command)}")
 
-
-def backup_mysql_databases(container_name):
+def backup_mysql(container_name):
     try:
         output = subprocess.check_output(["lxc", "exec", container_name, "--", "bash", "-c",
                                           "mysql -e 'show databases;'"]).decode()
         databases = output.split("\n")[1:-1]  # Remove a primeira e a última linha
 
         for db in databases:
-            if db not in excluded_mysql_databases:
-                timestamp = datetime.now().strftime("%d-%m-%Y")
-                backup_file = f"bk_{db}_{timestamp}.sql.gz"
-                backup_path = os.path.join(local_backup_dir, backup_file)
-                subprocess.run(["lxc", "exec", container_name, "--", "bash", "-c",
-                                f"mysqldump {db} | gzip > {backup_path}"])
-                print(f"Backup do banco de dados {db} concluído com sucesso.")
+            if db not in excluded_databases["mysql"]:
+                backup_database(container_name, "mysql", db)
     except subprocess.CalledProcessError:
-        print(f"Erro ao fazer backup do banco de dados no container {container_name}.")
+        print(f"Erro ao fazer backup do MySQL no container {container_name}.")
+
+def backup_postgres(container_name):
+    try:
+        output = subprocess.check_output(["lxc", "exec", container_name, "--", "bash", "-c",
+                                          "psql -U postgres -l | awk '{print $1}' | grep -vE '(template0|template1|postgres)'"]).decode()
+        databases = output.split("\n")[2:-1]  # Remove as primeiras duas linhas e a última linha
+
+        for db in databases:
+            backup_database(container_name, "postgres", db)
+    except subprocess.CalledProcessError:
+        print(f"Erro ao fazer backup do Postgres no container {container_name}.")
+
+def backup_database(container_name, db_type, db_name):
+    try:
+        timestamp = datetime.now().strftime("%d-%m-%Y")
+        backup_file = f"bk_{db_name}_{timestamp}.sql.gz"
+        backup_path = os.path.join(local_backup_dir, backup_file)
+
+        if db_type == "mysql":
+            command = ["lxc", "exec", container_name, "--", "bash", "-c",
+                       f"mysqldump {db_name} | gzip > {backup_path}"]
+        elif db_type == "postgres":
+            command = ["lxc", "exec", container_name, "--", "bash", "-c",
+                       f"pg_dump -U postgres {db_name} | gzip > {backup_path}"]
+
+        run_command(command)
+        print(f"Backup do banco de dados {db_name} ({db_type}) no container {container_name} concluído com sucesso.")
+    except Exception as e:
+        print(f"Erro ao fazer backup do banco de dados {db_name} ({db_type}) no container {container_name}: {e}")
 
 def main():
     try:
         containers = subprocess.check_output(["lxc", "list", "--format", "csv", "--columns", "n"]).decode().split("\n")
         for container in containers:
             if container and container not in excluded_containers:
-                backup_mysql_databases(container)
-                backup_postgres_databases(container)
+                backup_mysql(container)
+                backup_postgres(container)
     except subprocess.CalledProcessError:
         print("Erro ao listar os containers.")
 
 def copy_backups_to_remote():
     try:
-        subprocess.run(["rsync", "-av", local_backup_dir + "/", "vsitoe@192.168.68.200:" + remote_backup_dir])
+        subprocess.run(["rsync", "-av", local_backup_dir + "/", "usuario@servidor_remoto:" + remote_backup_dir])
         print("Backups copiados com sucesso para o servidor remoto.")
     except subprocess.CalledProcessError:
         print("Erro ao copiar backups para o servidor remoto.")

b281803cc55d62d1a9bff5fab9a26bc002ebbf86
NelioChume <74329121+NelioChume@users.noreply.github.com>
2024-02-21 09:14:14 +0200

    new

--- bk/main.py
+++ bk/main.py
@@ -26,26 +26,41 @@ def run_command(command):
 
 def backup_mysql(container_name):
     try:
-        output = subprocess.check_output(["lxc", "exec", container_name, "--", "bash", "-c",
-                                          "mysql -e 'show databases;'"]).decode()
-        databases = output.split("\n")[1:-1]  # Remove a primeira e a última linha
-
-        for db in databases:
-            if db not in excluded_databases["mysql"]:
+        # Verificar se o comando mysql está disponível no container
+        output = subprocess.check_output(["lxc", "exec", container_name, "--", "which", "mysql"]).decode().strip()
+        if output:
+            print(f"Backup do MySQL no container {container_name} iniciado.")
+            for db in get_mysql_databases(container_name):
                 backup_database(container_name, "mysql", db)
+        else:
+            print(f"O comando mysql não está disponível no container {container_name}.")
     except subprocess.CalledProcessError:
-        print(f"Erro ao fazer backup do MySQL no container {container_name}.")
+        print(f"Erro ao verificar a disponibilidade do MySQL no container {container_name}.")
 
 def backup_postgres(container_name):
     try:
-        output = subprocess.check_output(["lxc", "exec", container_name, "--", "bash", "-c",
-                                          "psql -U postgres -l | awk '{print $1}' | grep -vE '(template0|template1|postgres)'"]).decode()
-        databases = output.split("\n")[2:-1]  # Remove as primeiras duas linhas e a última linha
-
-        for db in databases:
-            backup_database(container_name, "postgres", db)
+        # Verificar se o comando psql está disponível no container
+        output = subprocess.check_output(["lxc", "exec", container_name, "--", "which", "psql"]).decode().strip()
+        if output:
+            print(f"Backup do Postgres no container {container_name} iniciado.")
+            for db in get_postgres_databases(container_name):
+                backup_database(container_name, "postgres", db)
+        else:
+            print(f"O comando psql não está disponível no container {container_name}.")
     except subprocess.CalledProcessError:
-        print(f"Erro ao fazer backup do Postgres no container {container_name}.")
+        print(f"Erro ao verificar a disponibilidade do Postgres no container {container_name}.")
+
+def get_mysql_databases(container_name):
+    output = subprocess.check_output(["lxc", "exec", container_name, "--", "bash", "-c",
+                                      "mysql -e 'show databases;'"]).decode()
+    databases = output.split("\n")[1:-1]  # Remove a primeira e a última linha
+    return [db for db in databases if db not in excluded_databases["mysql"]]
+
+def get_postgres_databases(container_name):
+    output = subprocess.check_output(["lxc", "exec", container_name, "--", "bash", "-c",
+                                      "psql -U postgres -l | awk '{print $1}' | grep -vE '(template0|template1|postgres)'"]).decode()
+    databases = output.split("\n")[2:-1]  # Remove as primeiras duas linhas e a última linha
+    return databases
 
 def backup_database(container_name, db_type, db_name):
     try:
@@ -84,4 +99,3 @@ def copy_backups_to_remote():
 
 if __name__ == "__main__":
     main()
-    copy_backups_to_remote()
